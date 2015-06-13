96d8b2124543332cf6458d1b5fbb8d502e2e75eb
Carter De Angelis <me@carterdea.com>
2015-06-13 10:24:50 -0700

    Adding a test for sign up

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,6 +7,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", contact_path    
+    get signup_path
+    assert_select "title", full_title("Sign Up")
+  end
+
+  test "full title helper" do
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
   end
 end
