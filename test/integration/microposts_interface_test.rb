6a1e1ca963cdf653d5c50ac22e85a76bf8394539
Carter De Angelis <me@carterdea.com>
2015-06-16 20:14:42 -0700

    Adding a test for images, splitting up the home pages

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,14 +9,17 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'ul.pagination'
+    assert_select 'input[type=file]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: "" }
     end
     assert_select 'div#error_explanation'
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('text/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count' do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -27,5 +30,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     end
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
